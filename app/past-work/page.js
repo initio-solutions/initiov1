d00159af3923ad40142bd3fc8c8b8b538f0e80e3
sowmya-subramaniam8 <sowmya.subbu8@gmail.com>
2022-11-13 16:56:00 +0000

    Initial Code Structure

---
+++ app/past-work/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PastWork() {
+  return <div>PastWork</div>;
+}
---
+++ components/form/textfield.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function textfield() {
+  return <div>textfield</div>;
+}
