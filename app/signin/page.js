c209c3cb108db4c53938f3a6eec83906b9d7f330
sowmya-subramaniam8 <sowmya.subbu8@gmail.com>
2022-11-13 17:11:32 +0000

    Initial textfield

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,5 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import TextField from "../../components/form/textfield";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [name, setName] = useState("");
+  const onChange = (e) => {
+    setName(e.target.value);
+  };
+  return (
+    <div>
+      <h1>SignIn</h1>
+      <TextField
+        value={name}
+        onChange={onChange}
+        type='text'
+        label={"Full name"}
+      />
+    </div>
+  );
 }
--- components/form/textfield.js
+++ components/form/textfield.js
@@ -1,5 +1,27 @@
 import React from "react";
 
-export default function textfield() {
-  return <div>textfield</div>;
+export default function TextField({ label, value, onChange, type }) {
+  return (
+    <div class='w-full max-w-lg'>
+      <div class='md:flex md:items-center mb-6'>
+        <div class='md:w-1/3'>
+          <label
+            class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+            for='grid-full-name'
+          >
+            {label}
+          </label>
+        </div>
+        <div class='md:w-2/3'>
+          <input
+            class='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+            id='inline-full-name'
+            type={type}
+            value={value}
+            onChange={onChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
