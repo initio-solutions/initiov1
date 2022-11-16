49005ecf056fd6cf29375d37dfe1440f2e987afc
Karthik Nishanth <karthik.nishanth06@gmail.com>
2022-11-16 15:49:12 +0000

    basic layout

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,14 @@
 import "../styles/globals.css";
 import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-
       <body>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- components/navbar.js
+++ components/navbar.js
@@ -1,100 +1,63 @@
 "use client";
-import { useState } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import Link from "next/link";
-const drawerWidth = 240;
-const navItems = ["About", "Careers", "Services", "Contact"];
-function Navbar(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = useState(false);
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
+function Navbar() {
   return (
-    <Box sx={{ display: "flex" }}>
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            <Link href="/"> Initio Solutions </Link>
-          </Typography>
-
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Link href={item === "Home" ? "/" : item.toLowerCase()}>
-                <Button key={item} sx={{ color: "#fff" }}>
-                  {item}
-                </Button>
-              </Link>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+          <Link href="/about" className="mr-5 hover:text-gray-900">
+            About
+          </Link>
+          <Link href="/blog" className="mr-5 hover:text-gray-900">
+            Blog
+          </Link>
+          <Link href="/careers" className="mr-5 hover:text-gray-900">
+            Careers
+          </Link>
+          <Link href="/past-work" className="hover:text-gray-900">
+            Past Work
+          </Link>
+        </nav>
+        <Link
+          href="/"
+          className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0"
         >
-          {drawer}
-        </Drawer>
-      </Box>
-    </Box>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span className="ml-3 text-xl">Initio Solutions</span>
+        </Link>
+        <div className="lg:w-2/5 inline-flex items-center lg:justify-end ml-5 lg:ml-0">
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Sign in
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+
+          <Link href="/contact" className="hover:text-gray-900 ml-2">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </header>
   );
 }
 
