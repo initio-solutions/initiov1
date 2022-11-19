8e8c97e52fbfdf3aca35012393b06d3c1dc51222
Karthik Nishanth <karthik.nishanth06@gmail.com>
2022-11-19 18:47:10 +0000

    firebased

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import TextField from "../../components/form/textfield";
 
 export default function SignIn() {
   const [name, setName] = useState("");
@@ -10,12 +9,6 @@ export default function SignIn() {
   return (
     <div>
       <h1>SignIn</h1>
-      <TextField
-        value={name}
-        onChange={onChange}
-        type='text'
-        label={"Full name"}
-      />
     </div>
   );
 }
