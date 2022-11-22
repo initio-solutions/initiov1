3754f86ce6fa3d3aaaf6fc88235c5fa6f072771e
Karthik Nishanth <karthik.nishanth06@gmail.com>
2022-11-22 13:25:43 +0000

    env error

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -7,16 +7,16 @@ export default async function handler(req, res) {
   switch (method) {
     case "GET":
       try {
-        const pets = await Contact.find({});
-        res.status(200).json({ success: true, data: pets });
+        const contacts = await Contact.find({});
+        res.status(200).json({ success: true, data: contacts });
       } catch (error) {
         res.status(400).json({ success: false });
       }
       break;
     case "POST":
       try {
-        const pet = await Contact.create(req.body);
-        res.status(201).json({ success: true, data: pet });
+        const contact = await Contact.create(req.body);
+        res.status(201).json({ success: true, data: contact });
       } catch (error) {
         res.status(400).json({ success: false });
       }
