4dac6bab8ce182e01d554c8aa4dad2c96c4ddcd4
Karthik Nishanth <karthik.nishanth06@gmail.com>
2022-11-24 14:53:47 +0000

    front end changes

---
+++ components/logo.js
@@ -0,0 +1,13 @@
+import Initio from "../assets/initio.png";
+function Logo({ size }) {
+  return (
+    <>
+      <img src={Initio.src} width={size} className="rounded" />
+      <p className="logo text-3xl p-2 font-extrabold">
+        Initio<span className="primary"> Solutions</span>
+      </p>
+    </>
+  );
+}
+
+export default Logo;
