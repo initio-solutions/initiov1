cea40ecb45efbbfeba0d32f0671ede5d0bf41eb7
Karthik <karthik.nishanth06@gmail.com>
2022-12-03 17:20:55 +0000

    app deleted

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
+import hi from '../assets/hi.png'
 export default function page() {
   return (
     <>
       <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-12 items-center justify-center flex-col">
+        <div style={{backgroundColor:'#3581b8'}} className="container mx-auto flex md:flex-row flex-col px-5 py-12 items-center justify-center">
           <img
-            className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
-            alt="hero"
-            src="https://dummyimage.com/720x600"
+            className="lg:w-2/6 md:w-3/6 w-3/6 mb-10 object-cover object-center rounded"
+            alt="hero" 
+            src={hi.src}
           />
-          <div className="text-center lg:w-2/3 w-full">
-            <h1 className="sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Microdosing synth tattooed vexillologist tghbtr5
+          <div className="text-center text-white lg:w-2/3 w-full">
+            <h1 className="sm:text-4xl  text-3xl mb-4 font-extrabold">
+            The Technology Pioneer.
             </h1>
             <p className="mb-8 leading-relaxed">
-              Meggings kinfolk echo park stumptown DIY, kale chips beard
-              jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice
-              godard disrupt ramps hexagon mustache umami snackwave tilde
-              chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac
-              mlkshk freegan photo booth af fingerstache pitchfork.
+            We are specialists in web and app solutions that aim to transform new concepts into a driving force for your company. With us, you can turn any idea into a reality.
+
             </p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-yellow-400 rounded text-lg">
