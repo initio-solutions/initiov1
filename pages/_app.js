f43945689982d544ea0a26d5f9d4192423b46474
Karthik <karthik.nishanth06@gmail.com>
2022-12-08 16:29:09 +0000

    meta

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,19 @@ import "../styles/globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import ScrolltoTop from "../components/scrolltotop";
+import { useEffect } from "react";
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init(process.env.PIXEL);
+        ReactPixel.pageView();
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
   return (
     <>
       <Navbar />
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -9,30 +8,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Script
-          id="meta"
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function()
-    {n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1285986808472620');
-    fbq('track', 'PageView');`,
-          }}
-        ></Script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1285986808472620&ev=PageView&noscript=1"
-          />
-        </noscript>
-
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
