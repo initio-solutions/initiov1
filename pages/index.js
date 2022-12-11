5986331499bd5c56e8dd35ffff0ed368b9ec1359
Karthik <karthik.nishanth06@gmail.com>
2022-12-11 19:33:49 +0000

    more edits

--- pages/index.js
+++ pages/index.js
@@ -321,10 +321,10 @@ export default function Page() {
               Pricing
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-gray-500">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical.
+              Customisable package for your needs.
             </p>
             <div className="flex mx-auto border-2 border-yellow-200 rounded overflow-hidden mt-6">
-              <button className="py-1 px-4 bg-primary text-white focus:outline-none">
+              <button className="py-1 px-4 bg-primary text-black focus:outline-none">
                 Monthly
               </button>
               <button className="py-1 px-4 focus:outline-none">Annually</button>
@@ -334,10 +334,10 @@ export default function Page() {
             <div className="p-4 xl:w-1/4 md:w-1/2 w-full ">
               <div className="h-full p-6 bg-white rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
                 <h2 className="text-sm text-gray-700 tracking-widest mb-1 font-medium">
-                  START
+                  STARTER
                 </h2>
                 <h1 className="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">
-                  Free
+                  Basic
                 </h1>
                 <p className="flex items-center text-gray-600 mb-2">
                   <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -353,7 +353,7 @@ export default function Page() {
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
                   </span>
-                  Vexillologist pitchfork
+                  Portfolio Site
                 </p>
                 <p className="flex items-center text-gray-600 mb-2">
                   <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -369,7 +369,7 @@ export default function Page() {
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
                   </span>
-                  Tumeric plaid portland
+                  Contact us and Blog
                 </p>
                 <p className="flex items-center text-gray-600 mb-6">
                   <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -385,9 +385,9 @@ export default function Page() {
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
                   </span>
-                  Mixtape chillwave tumeric
+                  Affiliate Links and Emails
                 </p>
-                <button className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
+                {/* <button className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
                   Button
                   <svg
                     fill="none"
@@ -400,20 +400,20 @@ export default function Page() {
                   >
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
-                </button>
-                <p className="text-xs text-gray-500 mt-3">
+                </button> */}
+                {/* <p className="text-xs text-gray-500 mt-3">
                   Literally you probably haven &apos; t heard of them jean
                   shorts.
-                </p>
+                </p> */}
               </div>
             </div>
             <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
               <div className="h-full p-6 rounded-lg border-2 bg-white border-yellow-200 flex flex-col relative overflow-hidden">
-                <span className="bg-primary text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
+                <span className="bg-primary text-black px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
                   POPULAR
                 </span>
                 <h2 className="text-sm tracking-widest text-gray-700 mb-1 font-medium">
-                  PRO
+                  ECOMMERCE
                 </h2>
                 <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
                   <span>$38</span>
@@ -485,7 +485,7 @@ export default function Page() {
                   </span>
                   Mixtape chillwave tumeric
                 </p>
-                <button className="flex items-center mt-auto text-white bg-yellow-200 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">
+                {/* <button className="flex items-center mt-auto text-white bg-yellow-200 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">
                   Button
                   <svg
                     fill="none"
@@ -498,11 +498,11 @@ export default function Page() {
                   >
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
-                </button>
-                <p className="text-xs text-gray-500 mt-3">
+                </button> */}
+                {/* <p className="text-xs text-gray-500 mt-3">
                   Literally you probably haven &apos; t heard of them jean
                   shorts.
-                </p>
+                </p> */}
               </div>
             </div>
             <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
@@ -596,7 +596,7 @@ export default function Page() {
                   </span>
                   Mixtape chillwave tumeric
                 </p>
-                <button className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
+                {/* <button className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
                   Button
                   <svg
                     fill="none"
@@ -609,11 +609,11 @@ export default function Page() {
                   >
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
-                </button>
-                <p className="text-xs text-gray-500 mt-3">
+                </button> */}
+                {/* <p className="text-xs text-gray-500 mt-3">
                   Literally you probably haven &apos; t heard of them jean
                   shorts.
-                </p>
+                </p> */}
               </div>
             </div>
             <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
@@ -707,7 +707,7 @@ export default function Page() {
                   </span>
                   Mixtape chillwave tumeric
                 </p>
-                <button className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
+                {/* <button className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
                   Button
                   <svg
                     fill="none"
@@ -720,11 +720,11 @@ export default function Page() {
                   >
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
-                </button>
-                <p className="text-xs text-gray-500 mt-3">
+                </button> */}
+                {/* <p className="text-xs text-gray-500 mt-3">
                   Literally you probably haven &apos; t heard of them jean
                   shorts.
-                </p>
+                </p> */}
               </div>
             </div>
           </div>
