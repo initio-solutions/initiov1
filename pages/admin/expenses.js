b142e71b20fed89a8f9b4be7d275b84785e10a75
Karthik <karthik.nishanth06@gmail.com>
2022-12-14 13:21:02 +0000

    modal

--- pages/admin/expenses.js
+++ pages/admin/expenses.js
@@ -2,11 +2,17 @@ import Nav from "./nav";
 import { useState, useEffect } from "react";
 import { ImBin2 } from "react-icons/im";
 import { FiEdit2 } from "react-icons/fi";
+import Modal from "../../components/modal";
+import Head from "next/head";
 function Expenses() {
   const [name, setName] = useState("");
   const [amount, setAmount] = useState();
   const [data, setData] = useState([]);
-
+  const [open, setOpen] = useState(false);
+  const [edname, setEdName] = useState("");
+  const [edamount, setEdAmount] = useState("");
+  const [id, setId] = useState("");
+  const input = true;
   useEffect(() => {
     getData();
   }, []);
@@ -85,6 +91,7 @@ function Expenses() {
   };
   const handleEdit = async (id) => {
     try {
+      console.log(edamount, edname);
       await fetch("/api/expenses", {
         method: "PUT",
         headers: {
@@ -93,8 +100,8 @@ function Expenses() {
         },
         body: JSON.stringify({
           id,
-          name,
-          amount,
+          name: edname,
+          amount: edamount,
         }),
       })
         .then((res) => res.json())
@@ -102,13 +109,30 @@ function Expenses() {
           if (data.success === true) {
             getData();
           }
-        });
+        })
+        .then(() => setOpen(false));
     } catch (e) {
       console.log(e);
     }
   };
   return (
     <>
+      <Head>
+        <title>Expenses</title>
+      </Head>
+      {open && (
+        <Modal
+          open={open}
+          setOpen={setOpen}
+          input={input}
+          edamount={edamount}
+          edname={edname}
+          setEdAmount={setEdAmount}
+          setEdName={setEdName}
+          handleEdit={handleEdit}
+          id={id}
+        />
+      )}
       <div className="flex ">
         <Nav variable="expenses" />
         <div className="ml-4 mt-2 w-full p-2">
@@ -141,11 +165,17 @@ function Expenses() {
                 <p>{d?.name}</p>
                 <p>£{d?.amount}</p>{" "}
                 <FiEdit2
-                  className="text-green-400"
-                  onClick={() => handleEdit(d._id)}
+                  className="text-green-400 cursor-pointer"
+                  onClick={() => {
+                    setOpen(true);
+                    setEdName(d.name);
+                    setEdAmount(d.amount);
+                    setId(d._id);
+                    //  handleEdit(d._id)}
+                  }}
                 />
                 <ImBin2
-                  className="text-red-500"
+                  className="text-red-500 cursor-pointer"
                   onClick={() => handleDelete(d._id)}
                 />
               </div>
